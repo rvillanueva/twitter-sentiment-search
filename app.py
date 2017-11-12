a2c25fb0be622bc8e89572d18c0cd6221d3f5160
Ryan Villanueva <ryanvill@gmail.com>
2017-11-12 17:39:31 -0500

    Added tweet api

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from flask import Flask, render_template, jsonify
+import twitter, json
+from urllib import parse
+from application_only_auth import Client
+import os
+
+import imp
+try:
+    imp.find_module('config')
+    import config
+except ImportError:
+    print('No config, using environment variables')
+
+consumer_key = os.getenv('TWITTER_CONSUMER_KEY', config.TWITTER_CONSUMER_KEY)
+consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET', config.TWITTER_CONSUMER_SECRET)
+client = Client(consumer_key, consumer_secret)
 
 app = Flask(__name__)
 
@@ -7,11 +22,17 @@ def index():
     return render_template('index.html')
 
 @app.route('/api/posts', methods=['get'])
-def getPosts():
+def returnPosts():
     res = {}
-    res['posts'] = []
+    res['posts'] = getTweets(term='@barackobama')
     return jsonify(res)
 
+def getTweets(term):
+  query = { 'q': term }
+  qs = parse.urlencode(query)
+  url = 'https://api.twitter.com/1.1/search/tweets.json?' + qs
+  tweets = client.request(url)
+  return tweets
 
 if __name__ == '__main__':
     app.run()
