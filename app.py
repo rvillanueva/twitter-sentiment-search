5b2a064762b238f9da79a241041d5ea8006f3695
Ryan Villanueva <ryanvill@gmail.com>
2017-11-13 16:51:29 -0500

    Search more tweets in cycles

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import json, os
+from datetime import datetime
 from flask import Flask, render_template, jsonify, request
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import json
 from urllib import parse
 from application_only_auth import Client
-import os
 
 import imp
 try:
@@ -20,7 +20,7 @@ aws_access_secret = os.getenv('AWS_ACCESS_SECRET', config.AWS_ACCESS_SECRET)
 
 app = Flask(__name__)
 analyzer = SentimentIntensityAnalyzer()
-
+cycles = 2
 
 @app.route('/')
 def index():
@@ -34,10 +34,35 @@ def returnPosts():
     return jsonify(res)
 
 def getTweets(q):
-  query = { 'q': q, 'count': 100}
-  qs = parse.urlencode(query)
-  url = 'https://api.twitter.com/1.1/search/tweets.json?' + qs
-  return client.request(url)
+  res = {'statuses': []};
+  earliest = datetime.now()
+  for i in range(cycles):
+    callRes = getTweetsSingleCycle(q=q, untilDate=earliest)
+    if(hasattr(res, 'search_metadata') == False):
+      res['search_metadata'] = callRes['search_metadata']
+    res['statuses'] = res['statuses'] + callRes['statuses']
+    earliest = getEarliestTweetDate(tweets=res['statuses'])
+  if(len(res['statuses']) > 100):
+    res['statuses'] = res['statuses'][0:100]
+  return res
+
+def getTweetsSingleCycle(q, untilDate):
+    query = { 'q': q, 'count': 100}
+    if untilDate:
+      query['util'] = str(untilDate.year) + '-' + str(untilDate.month) + '-' + str(untilDate.day)
+    qs = parse.urlencode(query)
+    url = 'https://api.twitter.com/1.1/search/tweets.json?' + qs
+    return client.request(url)
+
+def getEarliestTweetDate(tweets):
+    earliest = None
+    for tweet in tweets:
+        datetimeDate = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        if(earliest == None):
+          earliest = datetime.now(datetimeDate.tzinfo)
+        elif(datetimeDate < earliest):
+            earliest = datetimeDate
+    return earliest
 
 def analyzeTweetSentiments(tweets):
     for tweet in tweets:
